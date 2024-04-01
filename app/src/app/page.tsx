79ad48fb37f2448b3b33413d2a9b0dff30828d22
Tomos Wells <tom@tomoswells.com>
2024-04-01 18:10:43 +0100

    Update

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,11 +4,11 @@ import NextLink from "next/link"
 
 export default function HomePage() {
   return (
-    <Card className="w-full before:![background-color:transparent] p-5" >
+    <Card className="w-full before:![background-color:transparent] !p-5" >
       <Heading size="6">Home</Heading>
       <Flex direction="column" gap="2" my="4">
         <Text>This site provides access to try out a variety of tools related to the subject of financial mathematics.</Text>
-        <Text>The source code to create the site is available on <Link href="https://github.com/tomjwells/finance">GitHub</Link>.</Text>
+        <Text>The code used to create the site is open source and available on <Link href="https://github.com/tomjwells/finance">GitHub</Link>.</Text>
         <br />
         <Text>
           The currently implemented topics are:
