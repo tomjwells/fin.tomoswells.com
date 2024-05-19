cd7e91d90ff7221af733ab38dde3088bcee8f44d
Tomos Wells <tom@tomoswells.com>
2024-05-19 17:09:02 +0100

    Add Inter

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,47 +1,42 @@
-import "~/styles/globals.css"
-import "@radix-ui/themes/styles.css"
+import '~/styles/globals.css'
+import '@radix-ui/themes/styles.css'
 
+import { Suspense } from 'react'
+import { Tabs, TabsList, TabsTrigger } from '~/shadcn/Tabs'
 
-import { Suspense } from "react"
-import { Tabs, TabsList, TabsTrigger } from "~/shadcn/Tabs"
+import { Inter } from 'next/font/google'
 
-import { Inter } from "next/font/google"
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
-
-import { Container, Flex, Theme } from "@radix-ui/themes"
-import { env } from "~/env"
-export const runtime = "edge"
-
+import { Container, Flex, Theme } from '@radix-ui/themes'
+import { env } from '~/env'
+export const runtime = 'edge'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
 })
 
 export const metadata = {
-  title: "Financial Mathematics Tools",
-  description: "Quantitative Finance Tools - created by TJW",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: 'Financial Mathematics Tools',
+  description: 'Quantitative Finance Tools - created by TJW',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+    <html lang='en' className='dark' style={{ colorScheme: 'dark' }}>
       {/* <body className={`font-sans ${inter.variable}`}> */}
-      <body className={`font-sans`}>
-        <Theme appearance="dark">
-          <div className="flex min-h-screen flex-col">
-            <span className="py-4 sm:py-8" />
+      <body className={inter.variable}>
+        <Theme appearance='dark'>
+          <div className='flex min-h-screen flex-col'>
+            <span className='py-4 sm:py-8' />
             <Nav />
-            <main className="flex-1">
-              <Container size="3" p="2">
-                <Flex direction="column" align="center" gap="4">
+            <main className='flex-1'>
+              <Container size='3' p='2'>
+                <Flex direction='column' align='center' gap='4'>
                   {children}
                 </Flex>
               </Container>
@@ -55,29 +50,34 @@ export default function RootLayout({
   )
 }
 
-
 function Nav() {
   return (
     <header className={`flex w-full flex-col-reverse sm:flex-row justify-center`}>
-      <div className="w-full container mx-0 flex sm:h-20 items-center justify-center sm:justify-between py-6 sm:mx-16 md:mx-24 md:max-w-2xl lg:mx-56 lg:max-w-5xl">
+      <div className='w-full container mx-0 flex sm:h-20 items-center justify-center sm:justify-between py-6 sm:mx-16 md:mx-24 md:max-w-2xl lg:mx-56 lg:max-w-5xl'>
         <Suspense>
-          <Tabs defaultValue="/" >
-            <TabsList className="flex-col sm:flex-row h-auto sm:h-9 gap-1.5 sm:gap-0">
-              <TabsTrigger value="/" className="w-full sm:w-auto"><HomeIcon /></TabsTrigger>
-              <TabsTrigger value="/derivatives" className="w-full sm:w-auto">Options Pricing</TabsTrigger>
-              <TabsTrigger value="/markowitz" className="w-full sm:w-auto">Modern Portfolio Theory</TabsTrigger>
-              {
-                env.NODE_ENV === "development" && (
-                  <TabsTrigger value="/timeseries" className="w-full sm:w-auto">Timeseries Forecasting</TabsTrigger>
-                )
-              }
+          <Tabs defaultValue='/'>
+            <TabsList className='flex-col sm:flex-row h-auto sm:h-9 gap-1.5 sm:gap-0'>
+              <TabsTrigger value='/' className='w-full sm:w-auto'>
+                <HomeIcon />
+              </TabsTrigger>
+              <TabsTrigger value='/derivatives' className='w-full sm:w-auto'>
+                Options Pricing
+              </TabsTrigger>
+              <TabsTrigger value='/markowitz' className='w-full sm:w-auto'>
+                Modern Portfolio Theory
+              </TabsTrigger>
+              {env.NODE_ENV === 'development' && (
+                <TabsTrigger value='/timeseries' className='w-full sm:w-auto'>
+                  Timeseries Forecasting
+                </TabsTrigger>
+              )}
             </TabsList>
           </Tabs>
         </Suspense>
       </div>
-      <div className="flex justify-center sm:justify-end sm:mr-20">
-        <a href="https://github.com/tomjwells/finance" target="_blank" rel="noopener noreferrer">
-          <img src="/github-mark-white.svg" alt="GitHub Logo" className="h-8 w-8 min-w-8" />
+      <div className='flex justify-center sm:justify-end sm:mr-20'>
+        <a href='https://github.com/tomjwells/finance' target='_blank' rel='noopener noreferrer'>
+          <img src='/github-mark-white.svg' alt='GitHub Logo' className='h-8 w-8 min-w-8' />
         </a>
       </div>
     </header>
@@ -85,5 +85,10 @@ function Nav() {
 }
 
 function HomeIcon() {
-  return <svg height="16px" width="16px" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"></path><path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"></path></svg>
+  return (
+    <svg height='16px' width='16px' stroke='currentColor' fill='currentColor' strokeWidth='0' viewBox='0 0 24 24' aria-hidden='true' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z'></path>
+      <path d='M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z'></path>
+    </svg>
+  )
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,46 +1,46 @@
-import { InfoCircledIcon } from "@radix-ui/react-icons"
-import { Flex, Heading, Text, Card, Link, Callout } from "@radix-ui/themes"
-import NextLink from "next/link"
-import { env } from "~/env";
-
+import { InfoCircledIcon } from '@radix-ui/react-icons'
+import { Flex, Heading, Text, Card, Link, Callout } from '@radix-ui/themes'
+import NextLink from 'next/link'
+import { env } from '~/env'
 
 export default function HomePage() {
   return (
-    <Card className="w-full before:![background-color:transparent] !p-5" >
-      <Heading size="6">Home</Heading>
-      <Flex direction="column" gap="2" my="4">
-        <Text>This site provides access to try out a variety of tools related to the subject of financial mathematics.</Text>
-        <Text>The code used to create the site is open source and available on <Link href="https://github.com/tomjwells/finance">GitHub</Link>.</Text>
-        <br />
+    <Card className='w-full before:![background-color:transparent] !p-5'>
+      <Heading size='6'>Home</Heading>
+      <Flex direction='column' gap='2' my='4'>
+        <Text>This site hosts a variety of tools related to the subject of financial mathematics.</Text>
         <Text>
-          The currently implemented topics are:
+          The code used to create the site is open source and available on <Link href='https://github.com/tomjwells/finance'>GitHub</Link>.
         </Text>
-        <ul className="list-disc my-3 ml-6">
-          <li >
-            <Link asChild><NextLink href="/derivatives" >Options Pricing</NextLink></Link>
+        <br />
+        <Text>The currently implemented topics are:</Text>
+        <ul className='list-disc my-3 ml-6'>
+          <li>
+            <Link asChild>
+              <NextLink href='/derivatives'>Options Pricing</NextLink>
+            </Link>
           </li>
-          <li >
-            <Link asChild><NextLink href="/markowitz">Modern Portfolio Theory</NextLink></Link>
+          <li>
+            <Link asChild>
+              <NextLink href='/markowitz'>Modern Portfolio Theory</NextLink>
+            </Link>
           </li>
-          {
-            env.NODE_ENV === "development" && (
-              <li >
-                <Link asChild><NextLink href="/timeseries">Timeseries Forecasting</NextLink></Link>
-              </li>
-            )
-          }
+          {env.NODE_ENV === 'development' && (
+            <li>
+              <Link asChild>
+                <NextLink href='/timeseries'>Timeseries Forecasting</NextLink>
+              </Link>
+            </li>
+          )}
         </ul>
 
-        <Callout.Root variant="surface">
+        <Callout.Root variant='surface'>
           <Callout.Icon>
             <InfoCircledIcon />
           </Callout.Icon>
-          <Callout.Text>
-            None of the information on this website should be interpreted as financial or investment advice.
-          </Callout.Text>
+          <Callout.Text>None of the information on this website should be interpreted as financial or investment advice.</Callout.Text>
         </Callout.Root>
-
       </Flex>
     </Card>
-  );
+  )
 }
